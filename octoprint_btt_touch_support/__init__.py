2d9a7538a8d3719f232dfbdcfe8f31aa40523bdb
Jonathan Klimt <jonathan.klimt@rwth-aachen.de>
2024-11-24 10:18:44 +0100

    Modified the plugin for BTT Touchscreen support

--- octoprint_btt_touch_support/__init__.py
+++ octoprint_btt_touch_support/__init__.py
@@ -6,7 +6,6 @@ import logging
 import octoprint.plugin
 from octoprint.events import Events
 from octoprint.printer import PrinterCallback
-from flask_babel import gettext
 
 
 logger = logging.getLogger(__name__)
@@ -28,23 +27,23 @@ class ProgressMonitor(PrinterCallback):
         self.time_left_s = data["progress"]["printTimeLeft"]
 
 
-class M73progressPlugin(
+class BTT_Touch_Support(
     octoprint.plugin.ProgressPlugin,
     octoprint.plugin.EventHandlerPlugin,
     octoprint.plugin.StartupPlugin,
     octoprint.plugin.TemplatePlugin,
     octoprint.plugin.SettingsPlugin,
-    octoprint.plugin.RestartNeedingPlugin
+    octoprint.plugin.RestartNeedingPlugin,
 ):
     def on_after_startup(self):
         self._progress = ProgressMonitor()
         self._printer.register_callback(self._progress)
 
         settings = self._settings
-        self.output_time_left = settings.get_boolean(["output_time_left"])
         self.progress_from_time = settings.get_boolean(["progress_from_time"])
 
     def on_event(self, event, payload):
+        self._logger.debug("layer progress changed")
         if event == Events.PRINT_STARTED or event == Events.PRINT_DONE:
             # Firmware manages progress bar when printing from SD card
             if payload.get("origin", "") == "sdcard":
@@ -55,6 +54,12 @@ class M73progressPlugin(
             self._set_progress(0)
         elif event == Events.PRINT_DONE:
             self._set_progress(100, 0)
+        elif event == "DisplayLayerProgress_layerChanged":
+            cmd = "M118 P0 A1 action:notification Layer Left {}/{}".format(
+                payload.get("currentLayer", "0"), payload.get("totalLayer", "0")
+            )
+            self._logger.debug("layer progress changed: {}".format(cmd))
+            self._printer.commands([cmd])
 
     def on_print_progress(self, storage, path, progress):
         if not self._printer.is_printing():
@@ -65,16 +70,12 @@ class M73progressPlugin(
             return
 
         progress = 0.0
-        time_left = None
-
-        if self.output_time_left and self._progress.time_left_s is not None:
-            # M73 expects time left value in minutes, not seconds
-            time_left = self._progress.time_left_s / 60
+        time_left = self._progress.time_left_s / 60
 
         if (
-            self.progress_from_time and
-            self._progress.time_left_s is not None and
-            self._progress.time_elapsed_s is not None
+            self.progress_from_time
+            and self._progress.time_left_s is not None
+            and self._progress.time_elapsed_s is not None
         ):
             time_left_s = self._progress.time_left_s
             time_elapsed_s = self._progress.time_elapsed_s
@@ -87,62 +88,76 @@ class M73progressPlugin(
 
     def _set_progress(self, progress, time_left=None):
         if time_left is None:
-            gcode = "M73 P{:.0f}".format(progress)
+            gcode1 = "M118 P0 A1 action:notification Time Left 00h00m00s"
         else:
-            gcode = "M73 P{:.0f} R{:.0f}".format(progress, time_left)
+            gcode1 = "M118 P0 A1 action:notification Time Left {:02.0f}h{:02.0f}m{:02.0f}s".format(
+                time_left / 60, time_left % 60, self._progress.time_left_s % 60
+            )
 
-        self._printer.commands(gcode)
+        gcode2 = "M118 P0 A1 action:notification Data Left {:.0f}/100".format(progress)
+
+        self._printer.commands([gcode1, gcode2])
 
     def get_settings_defaults(self):
-        return dict(
-            output_time_left=True,
-            progress_from_time=False
-        )
+        return dict(progress_from_time=False)
 
     def on_settings_save(self, data):
         octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
 
         settings = self._settings
-        self.output_time_left = settings.get_boolean(["output_time_left"])
         self.progress_from_time = settings.get_boolean(["progress_from_time"])
 
     def get_template_configs(self):
         return [
             dict(
                 type="settings",
-                name=gettext("M73 Progress"),
-                custom_bindings=False
+                name="BTT TFT Touchscreen",
+                custom_bindings=False,
             )
         ]
 
     def get_update_information(self):
         return dict(
-            m73progress=dict(
-                displayName="M73 Progress Plugin",
+            btt_touch_support=dict(
+                displayName="BTT TFT Touchscreen support",
                 displayVersion=self._plugin_version,
-
                 # version check: github repository
                 type="github_release",
-                user="cesarvandevelde",
-                repo="OctoPrint-M73Progress",
+                user="jounathaen",
+                repo="OctoPrint-BTT_Touch_Support",
                 current=self._plugin_version,
-
                 # update method: pip
-                pip="https://github.com/cesarvandevelde/OctoPrint-M73Progress/archive/{target_version}.zip"
+                pip="https://github.com/jounathaen/octoprint_btt_touch_support/archive/{target_version}.zip"
             )
         )
 
+    def hook_actioncommands(self, comm, line, command, *args, **kwargs):
+        self._logger.debug("Command received: 'action:%s'" % (command))
 
-__plugin_name__ = "M73 Progress Plugin"
+        if command == None:
+            return
+
+        if command == "notification remote pause":
+            self._logger.debug("pausing")
+            self._printer.pause_print()
+        if command == "notification remote resume":
+            self._logger.debug("resuming")
+            self._printer.resume_print()
+        if command == "notification remote cancel":
+            self._logger.debug("canceling")
+            self._printer.cancel_print()
+
+
+__plugin_name__ = "BTT TFT TouchScreen Support"
 __plugin_pythoncompat__ = ">=2.7,<4"
 
 
 def __plugin_load__():
     global __plugin_implementation__
-    __plugin_implementation__ = M73progressPlugin()
+    __plugin_implementation__ = BTT_Touch_Support()
 
     global __plugin_hooks__
     __plugin_hooks__ = {
-        "octoprint.plugin.softwareupdate.check_config":
-            __plugin_implementation__.get_update_information
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+        "octoprint.comm.protocol.action": __plugin_implementation__.hook_actioncommands,
     }
